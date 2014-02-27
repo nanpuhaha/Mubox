6f7d0c09e40d1a2854548b5ce4957c546101a64e
wilson0x4d <mrshaunwilson@msn.com>
2014-02-26 17:15:57 -0800

    add 'InputDebugTool' to project

---
+++ InputDebugView/MainWindow.xaml.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace InputDebugTool
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Mubox.Configuration.KeySettingCollection keysettings;
+
+        private void Window_KeyUpDown(object sender, KeyEventArgs e)
+        {
+            var vk = (Mubox.WinAPI.VK)System.Windows.Input.KeyInterop.VirtualKeyFromKey(e.Key);
+            var key = keysettings.GetOrCreateNew(vk);
+            key.ActiveClientOnly = e.IsDown;
+            keys.InitializeButtonState(
+                keysettings,
+                (k) => k.ActiveClientOnly,
+                (k) =>
+                {
+                },
+                (k) =>
+                {
+                });
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            keysettings = new Mubox.Configuration.KeySettingCollection();
+        }
+
+        private void Window_MouseMove(object sender, MouseEventArgs e)
+        {
+            var position = e.GetPosition(canvas);
+            mouse.SetValue(Canvas.LeftProperty, position.X - 16);
+            mouse.SetValue(Canvas.TopProperty, position.Y - 16);
+            mouse.SetValue(Canvas.LeftProperty, position.X - 16);
+            mouse.SetValue(Canvas.TopProperty, position.Y - 16);
+        }
+
+        private void Window_MouseUpDown(object sender, MouseButtonEventArgs e)
+        {
+            var ellipse = default(Ellipse);
+            switch (e.ChangedButton)
+            {
+                case MouseButton.Left:
+                    ellipse = lmb;
+                    break;
+                case MouseButton.Middle:
+                    ellipse = mmb;
+                    break;
+                case MouseButton.Right:
+                    ellipse = rmb;
+                    break;
+                case MouseButton.XButton1:
+                    ellipse = xb1;
+                    break;
+                case MouseButton.XButton2:
+                    ellipse = xb2;
+                    break;
+            }
+            switch (e.ButtonState)
+            {
+                case MouseButtonState.Pressed:
+                    ellipse.SetValue(UIElement.VisibilityProperty, Visibility.Visible);
+                    break;
+                case MouseButtonState.Released:
+                    ellipse.SetValue(UIElement.VisibilityProperty, Visibility.Hidden);
+                    break;
+            }
+        }
+    }
+}
