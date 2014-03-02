d8795b00cd75f1771e602568ca09dc6785791570
Shaun Wilson <mrshaunwilson@msn.com>
2014-03-01 20:29:56 -0800

    add new bvt to test for issue on x64 PA

---
+++ Mubox.Tests/Toolhelp32Tests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mubox.Tests
+{
+    [TestClass]
+    public class Toolhelp32Tests
+    {
+        [TestMethod]
+        public void Toolhelp32BVT()
+        {
+            var result = WinAPI.Toolhelp32.GetChildProcesses(System.Diagnostics.Process.GetCurrentProcess().Id);
+            Assert.IsNotNull(result);
+        }
+    }
+}
